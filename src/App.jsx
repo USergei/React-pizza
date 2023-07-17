a48fbf7201c3428370b8274bd4729c3219937f96
USergei <serega.umerenkov@gmail.com>
2023-07-17 16:54:55 +0600

    Created the main components for an online pizza store. Created files with styles and installed the sass library. Installed the react-router-dom and react-content-loader libraries.

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from 'react-router-dom'
+
+import Header from './components/Header'
+import Home from './Pages/Home'
+import Cart from './Pages/Cart'
+import NotFound from './Pages/NotFound'
+
+import './scss/app.scss'
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </div>
+  )
+}
+
+export default App
